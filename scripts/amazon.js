7acea58018cf95259358c127ab94f2b6fcb4be45
Eugene <evg.v38@gmail.com>
2024-10-23 22:58:48 +1000

    * product name on id

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,7 +46,7 @@ products.forEach(product => {
 				Added
 			</div>
 
-			<button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
+			<button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
 				Add to Cart
 			</button>
 		</div>
@@ -57,12 +57,12 @@ document.querySelector('.js-product-grid').innerHTML = productsHTML
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
 	button.addEventListener('click', () => {
-		const productName = button.dataset.productName;
+		const productId = button.dataset.productId;
 
 		let matchingItem;
 
 		cart.forEach((item) => {
-			if (productName === item.productName) {
+			if (productId === item.productId) {
 				matchingItem = item;
 			}
 		})
@@ -71,7 +71,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
 			matchingItem.quantity += 1;
 		} else {
 			cart.push({
-				productName: productName,
+				productId: productId,
 				quantity: 1
 			})
 		}
