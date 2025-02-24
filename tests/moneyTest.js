2252dad0100b0f0c784bb4f063201b12d9694283
Eugene <evg.v38@gmail.com>
2025-02-24 22:37:25 +1000

    + automated tests for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,32 @@
+import { formatCurrency } from '../scripts/utils/money.js'
+
+console.log('test suite: formatCurrency');
+
+console.log('converts cents into dollars:');
+
+formatCurrency(2095) === '20.95'
+	? console.log('passed')
+	: console.log('failed');
+
+
+console.log('work with 0:');
+
+formatCurrency(0) === '0.00'
+	? console.log('passed')
+	: console.log('failed');
+
+
+console.log('rounds up to the nearest cent:');
+
+formatCurrency(2000.5) === '20.01'
+	? console.log('passed')
+	: console.log('failed');
+
+
+console.log('rounds down to the nearest cent:');
+
+formatCurrency(2000.4) === '20.00'
+	? console.log('passed')
+	: console.log('failed');
+
+
